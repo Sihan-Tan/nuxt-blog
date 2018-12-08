2120ca643046ddac5840153229545eedf8ce99a7
Orialian <tanxuan_2013@163.com>
2018-12-08 17:05:50 +0800

    f

---
+++ plugins/fullpage.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import 'velocity-animate'
+import 'hammerjs'
+import 'ks-vue-fullpage/dist/ks-vue-fullpage.min.css'
+import KsVueFullpage from 'ks-vue-fullpage'
+
+export default () => {
+  Vue.use(KsVueFullpage)
+}
